abe8c56aaed78296637879c8a1b6bf92036917b3
dinhlong1999 <long.truong.vcr@gmail.com>
2023-09-27 11:40:32 +0700

    oke

--- parkingcar/src/main/java/com/parkingcar/model/customer/Customer.java
+++ parkingcar/src/main/java/com/parkingcar/model/customer/Customer.java
@@ -20,11 +20,14 @@ public class Customer {
     private int id;
     private String name;
     private String phoneNumber;
+    private String licensePlates;
+    private LocalDate timeRemaining;
+    private int quantitySlot;
+    private String locationRent;
     private String roomRented;
     private String address;
-    private String avatar;
-    private int gender;
-    private LocalDate DOB;
+    private String images;
+    private int status;
     @ManyToOne()
     @JoinColumn(name = "packageRent_Id", referencedColumnName = "id")
     private PackageRent packageRent;
@@ -32,5 +35,24 @@ public class Customer {
     @OneToOne
     @JoinColumn(name = "account_id", referencedColumnName = "id")
     private Account account;
+    @OneToMany(mappedBy = "customer", cascade = CascadeType.ALL)
+    private List<ParkingLot> parkingLots;
+//    @Id
+//    @GeneratedValue(strategy = GenerationType.IDENTITY)
+//    private int id;
+//    private String name;
+//    private String phoneNumber;
+//    private String roomRented;
+//    private String address;
+//    private String avatar;
+//    private int gender;
+//    private LocalDate DOB;
+//    @ManyToOne()
+//    @JoinColumn(name = "packageRent_Id", referencedColumnName = "id")
+//    private PackageRent packageRent;
+//
+//    @OneToOne
+//    @JoinColumn(name = "account_id", referencedColumnName = "id")
+//    private Account account;
 
 }
