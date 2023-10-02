bcdeb40035501ff7b1234362046e8902baf2a9c9
dinhlong1999 <long.truong.vcr@gmail.com>
2023-10-02 11:46:38 +0700

    longupdate

--- parkingcar/src/main/java/com/parkingcar/repository/customer/ICustomerRepository.java
+++ parkingcar/src/main/java/com/parkingcar/repository/customer/ICustomerRepository.java
@@ -2,7 +2,10 @@ package com.parkingcar.repository.customer;
 
 import com.parkingcar.model.customer.Customer;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
 
 public interface ICustomerRepository extends JpaRepository<Customer, Integer> {
-
+        Customer findCustomerByAccount_Id(int id);
 }
