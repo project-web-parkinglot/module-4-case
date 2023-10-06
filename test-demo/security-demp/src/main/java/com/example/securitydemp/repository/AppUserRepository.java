9c457ef2e484ef783b68556f2af3c36ec9e92c20
dinhlong1999 <long.truong.vcr@gmail.com>
2023-10-06 08:34:00 +0700

    longupdate

--- test-demo/security-demp/src/main/java/com/example/securitydemp/repository/AppUserRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.securitydemp.repository;
-
-import com.example.securitydemp.model.AppUser;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface AppUserRepository extends JpaRepository<AppUser, Long> {
-    AppUser findByUserName(String userName);
-}
